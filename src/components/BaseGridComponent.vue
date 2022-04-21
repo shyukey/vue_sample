4b1bcdcd337907fc1d23a41567f8943a85eda72b
yukey <yukey6428@gmail.com>
2022-04-21 18:53:19 +0900

    first-commit

---
+++ src/components/BaseGridComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <label>test</label>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "BaseGridComponent",
+  components: {
+  },
+  props: {},
+  data() {
+    return {
+      cols: ["Make", "Model", "Year", "Color"],
+      rows: [
+        ["Ford", "Fusion", "2011", "Silver"],
+        ["Chevrolet", "Cruz", "2018", "White"],
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'HeaderComponent',
+  components: {
+  },
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
